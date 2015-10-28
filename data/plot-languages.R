4203fc9969deb46649b4555503fde8a07754c03e
JJ Merelo <jjmerelo@gmail.com>
2015-10-28 14:32:41 +0100

    Cambiando a gris

--- data/plot-languages.R
+++ data/plot-languages.R
@@ -5,5 +5,5 @@ language.top20 <- as.data.frame(summary(language.province$language, max=25))
 language.top20.df <- data.frame(language=row.names(language.top20),
                                 devs=language.top20$"summary(language.province$language, max = 25)")
 
-ggplot()+geom_bar(data=language.top20.df,aes(reorder(language,devs),y=devs),stat="identity")+coord_flip()
-ggplot( data=language.top20.df, aes(x="",y=devs,fill=factor(language)))+ geom_bar(width=1,stat='identity') + coord_polar(theta='y')
+ggplot()+geom_bar(data=language.top20.df,aes(reorder(language,devs),y=devs),stat="identity")+coord_flip()+theme_bw()
+ggplot( data=language.top20.df, aes(x="",y=devs,fill=factor(language)))+ geom_bar(width=1,stat='identity') + coord_polar(theta='y') + scale_fill_grey(start=0.1, end=1)+theme_bw()
